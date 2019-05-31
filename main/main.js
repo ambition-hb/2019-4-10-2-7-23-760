f2094e20c2c658afd68a0b7fb283ef58be4cb5b7
ambition-hb <157391873@qq.com>
2019-05-31 10:37:33 +0800

    submit taxi program

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var price;
+    var d = inputs.distance;
+    var time = inputs.parkTime;
+    if(d <= 2)
+    	price = 6 + time * 0.25;
+    else if (d < 8) {
+    	price = 6 + (d - 2) * 0.8 + time * 0.25;
+    }else{
+    	price = 6 + 6 * 0.8 + (d - 8) * 1.2 + time * 0.25;
+    }
+    return Math.round(price);
 };
